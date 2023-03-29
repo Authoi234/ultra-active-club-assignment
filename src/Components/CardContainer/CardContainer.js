bee70edeb914fb458883f5e5357dabb8d39b14f8
Authoi234 <jawadkhan.authoi@gmail.com>
2023-03-29 16:33:35 +0600

    responsived

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -42,35 +42,39 @@ const CardContainer = () => {
                     </div>
                 </div>
                 <div className='calculate'>
-                    <div className='d-flex padding'>
-                        <img className='myImg' src={myImg} alt="" />
-                        <div>
-                            <p><strong>Jawad Jabbar Khan Authoi</strong></p>
-                            <div className="d-flex">
-                                <FontAwesomeIcon icon={faLocationDot} />
-                                <span><strong>Brahmanbaria, Bangladesh</strong></span>
+                    <div>
+                        <div className='d-flex padding'>
+                            <img className='myImg' src={myImg} alt="" />
+                            <div>
+                                <p><strong>Jawad Jabbar Khan Authoi</strong></p>
+                                <div className="d-flex">
+                                    <FontAwesomeIcon icon={faLocationDot} />
+                                    <span><strong>Brahmanbaria, Bangladesh</strong></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='information'>
-                        <span>4F 10.6in <p>Height</p></span>
-                        <span>35Kg <p>Weight</p></span>
-                        <span>12<p>Years</p> </span>
-                    </div>
-                    <div className='break'>
-                        <h3>Add A Break</h3>
-                        <div className='break-container'>
-                            <div><span onClick={breakNum}>10</span><span>s</span></div><div><span onClick={breakNum}>20</span><span>s</span></div><div><span onClick={breakNum}>30</span><span>s</span></div><div><span onClick={breakNum}>40</span><span>s</span></div>
+                        <div className='information'>
+                            <span>4F 10.6in <p>Height</p></span>
+                            <span>35Kg <p>Weight</p></span>
+                            <span>12<p>Years</p> </span>
                         </div>
                     </div>
-                    <div className='details'>
-                        <h3>Exercise Details</h3>
-                        <div className='details-container'>
-                            <div><h5>Exercise time</h5> <p className='para-style'>{exerciseTime}s</p></div>
-                            <div><h5>Break time</h5> <p className='para-style'>{JSON.parse(localStorage.getItem('breakTimeNum'))}s</p></div>
+                    <div>
+                        <div className='break'>
+                            <h3>Add A Break</h3>
+                            <div className='break-container'>
+                                <div><span onClick={breakNum}>10</span><span>s</span></div><div><span onClick={breakNum}>20</span><span>s</span></div><div><span onClick={breakNum}>30</span><span>s</span></div><div><span onClick={breakNum}>40</span><span>s</span></div>
+                            </div>
                         </div>
+                        <div className='details'>
+                            <h3>Exercise Details</h3>
+                            <div className='details-container'>
+                                <div><h5>Exercise time</h5> <p className='para-style'>{exerciseTime}s</p></div>
+                                <div><h5>Break time</h5> <p className='para-style'>{JSON.parse(localStorage.getItem('breakTimeNum'))}s</p></div>
+                            </div>
+                        </div>
+                        <button className='completed-btn'>Activity Completed</button>
                     </div>
-                    <button className='completed-btn'>Activity Completed</button>
                 </div>
             </div>
         </div>

0e7f6a7987ce4b565caa27db7ef419d3056a54d0
Authoi234 <jawadkhan.authoi@gmail.com>
2023-03-29 16:38:44 +0600

    naved

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='position'>
+        <nav className='position'>
             <h2 className='header'><img className='logo' src={logo} alt="" /> <strong>DAILY EXERCISE</strong></h2>
             <ul>
                 <li><a href="https://stellular-selkie-4e641c.netlify.app/">Home</a></li>
@@ -12,7 +12,7 @@ const Header = () => {
                 <li><a href="https://stellular-selkie-4e641c.netlify.app/">Login</a></li>
                 <li><a href="https://stellular-selkie-4e641c.netlify.app/">Services</a></li>
             </ul>
-        </div>
+        </nav>
     );
 };
 
